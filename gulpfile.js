c861f927bfd3c2efe3bd8eb7f4a97c4bea02c47c
Januar Fonti <me@januarfonti.com>
2018-03-02 21:54:35 +0700

    Let's optimize css

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
+var useref = require('gulp-useref');
+var uglify = require('gulp-uglify');
+var gulpIf = require('gulp-if');
+var cssnano = require('gulp-cssnano');
+
 
 gulp.task('hello', function() {
   console.log('Halo gan!');
@@ -31,3 +36,12 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
   gulp.watch('app/js/**/*.js', browserSync.reload);
 })
 
+// Optimizing CSS and JavaScript 
+gulp.task('useref', function() {
+
+  return gulp.src('app/*.html')
+    .pipe(useref())
+    .pipe(gulpIf('*.js', uglify()))
+    .pipe(gulpIf('*.css', cssnano()))
+    .pipe(gulp.dest('dist'));
+});
