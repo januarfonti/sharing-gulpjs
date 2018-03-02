6f68cb1de474e75685d6988d8857e804599efb31
Januar Fonti <me@januarfonti.com>
2018-03-02 20:45:40 +0700

    Play with sass

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,12 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
 
 gulp.task('hello', function() {
   console.log('Halo gan!');
 });
+
+gulp.task('sass', function() {
+  return gulp.src('app/scss/**/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('app/css'))
+})
