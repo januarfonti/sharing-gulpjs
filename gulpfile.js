5e298424efd5a378ffad0129382a5245d8fb7ed7
Januar Fonti <me@januarfonti.com>
2018-03-02 23:28:13 +0700

    Optimize images

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var useref = require('gulp-useref');
 var uglify = require('gulp-uglify');
 var gulpIf = require('gulp-if');
 var cssnano = require('gulp-cssnano');
+var imagemin = require('gulp-imagemin');
 
 
 gulp.task('hello', function() {
@@ -44,4 +45,14 @@ gulp.task('useref', function() {
     .pipe(gulpIf('*.js', uglify()))
     .pipe(gulpIf('*.css', cssnano()))
     .pipe(gulp.dest('dist'));
+});
+
+// Optimizing Images 
+gulp.task('images', function() {
+  return gulp.src('app/images/**/*.+(png|jpg|jpeg|gif|svg)')
+    // Caching images that ran through imagemin
+    .pipe(imagemin({
+      interlaced: true,
+    }))
+    .pipe(gulp.dest('dist/images'))
 });
